aa545a09aff2ce5814ae17f7fd732bea66d51b07
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-07-06 00:10:17 -0700

    improving edge detection for unconstricted cell size

--- Portion_segment.m
+++ Portion_segment.m
@@ -26,8 +26,8 @@ smallest_cell = 5;
 % Emperical threshold value for binarizing images (default 4)
 threshold = 4;          
 folder_name = 'C:\Users\agopinath\Desktop\CellVideos\';
-video_name = ['compressed2.avi'];
-position = [14     19    529    285];
+video_name = 'compressed.avi';%'unconstricted_test.avi';
+% position = [5     5    540    300];
 seg_number = 1;
 
 % create the folder to write to
@@ -45,7 +45,12 @@ select_range = 1:ceil(temp_mov.NumberOfFrames/100):temp_mov.NumberOfFrames; % ch
 % Compiles Aavi, an array of 100 evenly spaced frames specified by
 % select_range, and then averages over RGB to get Aaviconverted (uint8 type
 % uses less memory)
-for i=1:length(select_range)
+temp = read(temp_mov, 1);
+height = size(temp, 1);
+width = size(temp, 2);
+
+Aaviconverted = zeros(height, width, 'uint8');
+for i = 1:length(select_range)
     Aavi = read(temp_mov, select_range(i));
     Aaviconverted(:,:,i) = uint8(mean(Aavi,3));
 end
@@ -54,25 +59,24 @@ end
 % value over the 100 selected frames.  Amean is the average of these 100
 % frames.  The 'max' and 'min' statements ensure the box (specified by the
 % user) are nonnegative and within the video size.
-for i = max([1 position(2)]):min([size(Aaviconverted,1) position(2)+position(4)])
-    for j = max([1 position(1)]):min([size(Aaviconverted,2) position(1)+position(3)])
-        Amean(i-max([1 position(2)])+1,j-max([1 position(1)])+1) = uint8(mean(Aaviconverted(i,j,:)));
+
+Amean = zeros(height, width, 'uint8');
+for i = 1:height
+    for j = 1:width
+        Amean(i,j) = uint8(mean(Aaviconverted(i,j,:)));
     end
 end
 
 %% Steps through the video one frame at a time to segment out cells
 % Clears variables to conserve memory 
-clear Aaviconverted; clear select_range;
+clear Aaviconverted; clear select_range; clear temp;
 
 for rep = start_frame:end_frame
     %% Reads in the movie file frame by frame
     Aavi = read(temp_mov, rep); 
 
     % Converts the Avi from a structure format to a 3D array (In future versions, speed can be improved of the code is altered to work on cell strct instead of 3D array.
-    Aaviconverted(:,:) = uint8((mean(Aavi,3)));
-    %     Aaviconvertedplacehold(:,:,i)= Aaviconverted(:,:,i);
-    %     figure(11);
-    %     imshow(Aaviconverted(:,:));
+    Aaviconverted(:,:) = uint8(mean(Aavi,3));
 
     % Clears Aavi to save memory, add more variables if you landup with 
     % Java heap or out of memory error. 
@@ -82,25 +86,33 @@ for rep = start_frame:end_frame
     % Subtracts the background (Amean) from each frame, hopefully leaving
     % just the cells.  Again, the min/max statements ensure the indicies
     % are nonzero.
-    Aaviconverted2(:,:) = (imsubtract(Amean,Aaviconverted(max([1 position(2)]):min([size(Aaviconverted,1) position(2)+position(4)]), max([1 position(1)]):min([size(Aaviconverted,2) position(1)+position(3)]))));   
-
+    Aaviconverted2 = imsubtract(Amean, Aaviconverted(:,:));
+    Aaviconverted2 = imadjust(Aaviconverted2);
+    seD = strel('disk', 1);
+    Aaviconverted2 = imerode(Aaviconverted2, seD);
+    %Aaviconverted2 = medfilt2(Aaviconverted2,[4, 4]);%imerode(Aaviconverted2, seD);
+    Aaviconverted2 = bwareaopen(Aaviconverted2, 40);
+    seD = strel('disk', 5);
+    Aaviconverted2 = imclose(Aaviconverted2, seD);
+    %myfilter = fspecial('gaussian',[3, 3], 4);
+    %Aaviconverted2 = imfilter(Aaviconverted2, myfilter, 'replicate');
     % Performs a bottom hat filter using the strel 'se'
-    se = strel('disk',BH_size);
-    BH(:,:) = imbothat(Aaviconverted2,se);
-
+    %se = strel('disk',BH_size);
+    %BH(:,:) = imbothat(Aaviconverted2,se);
+    
     % Performs a median filter using the pixel size specified in the
     % header
-    med_bhat(:,:) = medfilt2(BH(:,:),[med_size med_size]);
+    %med_bhat(:,:) = medfilt2(BH(:,:),[med_size med_size]);
 
     % Gets rid of small 'cells'.  This function gets rid of any connected
     % region which is less that smallest_cell pixels connected.  Each frame
     % is converted to black and white before the small cells are filled in
     % with black.
-    Clean(:,:) = bwareaopen(im2bw(double(med_bhat(:,:))/256, threshold/256),smallest_cell);
+    %Clean(:,:) = bwareaopen(im2bw(double(med_bhat(:,:))/256, threshold/256),smallest_cell);
 
     %% Save
     % The following code saves image sequence and the image template with
     % the demarcation lines for the transit time analysis.
     filename = [writeFolder, '\','BWstill_', num2str(rep),'.tif']; %% Change filename .tif
-    imwrite(Clean(:,:),filename,'Compression','none');
+    imwrite(Aaviconverted2(:,:),filename,'Compression','none');
 end
