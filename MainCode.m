ce136317a0c97374a2e15017e6b36d114c6891af
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-07-16 00:06:53 -0700

    major cleanup, commenting, and adding debugging information in MainCode

--- MainCode.m
+++ MainCode.m
@@ -1,4 +1,4 @@
-%% Data Compilation file; runs sequential segmentations and analyses; objective is minimal user input
+%% Code entry point; runs sequential segmentations and analyses; objective is minimal user input and to compile and output analysis data
 % Updated 7/09/2013 by Mike
 %       -Cut out the preprocessing 50 frames (required editing indicies of
 %       the call for CellDetection
@@ -7,88 +7,66 @@
 %       longer requires defining the cropping and constriction regions
 %       - Eliminated redundant inputs and outputs from functions
 %       - Eliminated 'segments', nobody used them
+% Updated 7/16/2013 by Ajay
+%       - separated all logic for prompting/selection of video files to
+%       process into function PromptForVideos
+%       - improved extraction of frame rates and template sizes from video
+%       names by using regular expressions instead of ad-hoc parsing
+%       - cleaned up any remaining legacy code and comments
+%       - added better output of debugging information
+close all
 clc
 
 %% Initializations
-filename = 1;
-filePath = 'G:\CellVideos\';
 % Allocates an array for the data
-compiledData = zeros(1,8);
+numDataCols = 8;
+compiledData = zeros(1, numDataCols);
 compiledDataPath = 'G:\CellVideos\';
 % Initializes a progress bar
 progressbar('Overall', 'Cell detection', 'Cell tracking');
 
-%% Loading GUI
-% Opens a GUI to select videos, user can select a single file at a time and
-% ends selection by clicking 'cancel'
-i = 1;
-j = 1;
-while (filename ~= 0)
-    [filename, filePath] = uigetfile('.avi', 'Please select the next video file, if done select cancel.', filePath, 'multiselect','on');
-    if size(filename,2) ~= 1
-        filename = cellstr(filename);
-        while j <= size(filename,2)
-            filename1 = filename{j};
-            videoNameLength(i) = length(filename1);
-            pathNameLength(i) = length(filePath);
-            videoNames(i,1:videoNameLength(i)) = filename1;
-            pathNames(i,1:pathNameLength(i)) = filePath;
-            i = i+1;
-            j = j+1;
-        end
-        j = 1;
-        filename = filename1;
-        clear filename1
-    end
-end
+%% Load video files and prepare any metadata
+[pathNames, videoNames] = PromptForVideos('G:\CellVideos\');
 
 % Reads title of fps from title of file
 % Important that fps come after first underscore
 % Title format to follow
 % 'dev5x10_1200fps_48hrppb_glass_4psi_20x_0.6ms_p12_041'
-for i = 1:size(videoNameLength, 2)
-    j = 1; ii = 1;
-    while videoNames(i,j) ~= 'f'
-        while videoNames(i,j) == '_'
-            if videoNames(i,k) == 'f'
-                break
-            end
-            frameRate(ii) = videoNames(i,k);
-            k = k+1;
-            ii = ii+1;
-        end
-        j = j+1;
-        k = j+1;
-    end
+for i = 1:length(videoNames)
+    videoName = videoNames{i};
+    underscores = regexp(videoName, '_'); % get indices of underscores present in filename
+    toParse = videoName(1:underscores(2)-1); % get everything up until the framerate
+    [j,k] = regexp(videoName, '\d*x'); % store start/end indices of template size
+    [m, n] = regexp(videoName, '\d*fps'); % store start/end indices of frame rate
+    templateSize = videoName(j:(k-1)); % 'k-1' removes the 'x' character at the end
+    frameRate = videoName(m:(n-3)); % 'n-3' to removes the 'fps' characters at the end
+    
+    templateSizes(i) = str2double(templateSize);
     frameRates(i) = str2double(frameRate);
-end
-
-%Reads template size from title
-%only works if one follows titling fomrat shown above
-for ii = 1:size(videoNameLength,2)
-    if size(videoNameLength,2) == 1
-        templateSize = str2double(videoNames(4));
-    else
-        templateSize(ii) = str2double(videoNames(ii,4));
-    end
 end
 
 tStart = tic;
 
-% Creates a directory for the compiled data
-mkdir(fullfile(compiledDataPath, 'compiled_data\'));
+% Creates a directory for the compiled data if it doesn't already exist
+if ~(exist(fullfile(compiledDataPath, 'compiled_data\'), 'file') == 7)
+    mkdir(fullfile(compiledDataPath, 'compiled_data\'));
+end
 
-for i = 1:size(videoNames,1)
+%% Iterates through videos to filter, analyze, and output the compiled data
+for i = 1:length(videoNames)
+    % Initializations
+    currPathName = pathNames{i};
+    currVideoName = videoNames{i};
+    currVideo = VideoReader(fullfile(currPathName, currVideoName));
+    startFrame = 1;
+    endFrame = currVideo.NumberOfFrames;
+    
+    disp(['==Video ', num2str(i), '==']);
+    
     % Calls the MakeWaypoints function to define the constriction region.
     % This function draws a template with a line across each constriction;
     % these lines are used in calculating the transit time
-    currPathName = pathNames(i,1:pathNameLength(i));
-    currVideoName = videoNames(i,1:videoNameLength(i));
-    
-    currVideo = VideoReader([currPathName, currVideoName]);
-    startFrame = 1;
-    endFrame = currVideo.NumberOfFrames;
-    [mask, lineTemplate, xOffset] = MakeWaypoints(currVideo, templateSize(i));
+    [mask, lineTemplate, xOffset] = MakeWaypoints(currVideo, templateSizes(i));
     
     % Calls CellDetection to filter the images and store them in
     % 'processedFrames'.  These stored image are binary and should
@@ -96,31 +74,38 @@ for i = 1:size(videoNames,1)
     [processedFrames] = CellDetection(currVideo, startFrame, endFrame, frameRates(i), currPathName, currVideoName, mask);
     progressbar((i/(2*size(videoNames,1))), [], [])
     
-    % Calls CellTracking to track the located cells.
+    % Calls CellTracking to track the detected cells.
     [data] = CellTracking((endFrame-startFrame+1), frameRates(i), lineTemplate, processedFrames, xOffset, currVideo);
     progressbar((i/(size(videoNames,1))), 0, 0)
     
     % If data is generated (cells are found and tracked through the device)
     if (~isempty(data))
         % If the first row is zeros (has not been written to yet)
-        if (compiledData(1,1:8) == zeros(1,8))
+        if (compiledData(1,1:numDataCols) == zeros(1,numDataCols))
             compiledData = data;
         % Otherwise add the new data
         else
-            compiledData(end+1:end+size(data,1),1:8) = data;
+            compiledData(end+1:end+size(data,1),1:numDataCols) = data;
         end
         
         % plot histogram of total transit times
-        figure(99)
+        figure('name', 'Total Transit times');
         [n,xout] = hist(compiledData(:,1));
         bar(xout,n)
         
         % Writes out the transit time data in an excel file
-        colHeader = {'Total Time (ms)', 'Constriction 1 to 2', 'Constriction 2 to 3', 'Constriction 3 to 4', 'Constriction 4 to 5', 'Constriction 5 to 6', 'Constriction 6 to 7'};
-        xlswrite(fullfile(compiledDataPath, 'compiled_data\data_xlscomp'), colHeader,'Sheet1','A1');
-        xlswrite(fullfile(compiledDataPath, 'compiled_data\data_xlscomp'), compiledData,'Sheet1','A2');
+        colHeader = {'Total Time (ms)', 'Constriction 1 to 2', 'Constriction 2 to 3', 'Constriction 3 to 4', ... 
+                    'Constriction 4 to 5', 'Constriction 5 to 6', 'Constriction 6 to 7', 'Unconstricted cell size (pixels)'};
+        xlswrite(fullfile(compiledDataPath, 'compiled_data\data_xlscomp'), colHeader, 'Sheet1', 'A1');
+        xlswrite(fullfile(compiledDataPath, 'compiled_data\data_xlscomp'), compiledData, 'Sheet1', 'A2');
     end
 end
 
-totalTime = toc(tStart)
-avgTimePerVideo = totalTime/size(videoNames,1)
+%% Output debugging information
+totalTime = toc(tStart);
+avgTimePerVideo = totalTime/length(videoNames);
+
+disp(sprintf('\n\n==========='));
+disp(['Total time to analyze ', num2str(length(videoNames)), ' video(s): ', num2str(totalTime), ' secs']);
+disp(['Average time per video: ', num2str(avgTimePerVideo), ' secs']);
+disp('');
