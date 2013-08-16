9b13804f0f208eca001a2fd409421c7a01d34f20
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-08-16 14:18:12 -0700

    added matching which considers all potential matches and then matches based on minimizing a score

--- CellTrackingEveryFrame.m
+++ CellTrackingEveryFrame.m
@@ -69,16 +69,16 @@ for currFrameIdx = 1:numFrames
         % Compute their centroids
         cellProps = regionprops(labeledFrame, 'centroid', 'area', 'BoundingBox',...
                                               'MajorAxisLength', 'MinorAxisLength');
+        potentialMatches = cell(1,16);
         
         %% Check which line the object intersects with
         for currCellIdx = 1:numLabels
             currCell = cellProps(currCellIdx);
-            [offCenter, currLane] = min(abs(laneCoords-currCell.Centroid(1)));
-            
+            [offCenter, cellLane] = min(abs(laneCoords-currCell.Centroid(1)));
             % Find the indices of all "open" cells - those which have
             % already hit the "start" tripwire but have not yet hit the
             % "end" tripwire (i.e. those which are still passing)
-            cellPassingIndices = find(passageStatus{currLane});
+            cellPassingIndices = find(passageStatus{cellLane});
             
             % If the cell centroid X coord is more than 12 pixels from the
             % nearest hardcoded lane X coord, it's too early to do any
@@ -90,58 +90,104 @@ for currFrameIdx = 1:numFrames
             end
             
             matchFound = false;
-            foundIndex = -1;
             for i = 1:length(cellPassingIndices)
-                checkCell = cellData{currLane}{cellPassingIndices(i)}(end, :);
-                if(currCell.Centroid(2) - (checkCell(3)-3) > 0 &&...
-                   ((abs(checkCell(4) - currCell.Area)) < 0.5*currCell.Area) &&...
+                checkCell = cellData{cellLane}{cellPassingIndices(i)}(end, :);
+                if(currCell.Centroid(2) - (checkCell(3)-5) > 0 &&...
                    (currFrameIdx > checkCell(1)))
-                    disp(['Found match in lane ', num2str(currLane), ': ']);
+%                    ((abs(checkCell(4) - currCell.Area)) < 0.5*checkCell(4)) &&
+                    disp(['Found match in lane ', num2str(cellLane), ': ']);
                     checkCell
                     [currFrameIdx, currCell.Centroid(1), currCell.Centroid(2), currCell.Area...
                      currCell.MajorAxisLength, currCell.MinorAxisLength]
                     matchFound = true;
-                    foundIndex = cellPassingIndices(i);
-                    break;
+                    len = length(potentialMatches{cellLane})+1;
+                    potentialMatches{cellLane}(len, 1) = checkCell(1);
+                    potentialMatches{cellLane}(len, 2) = currCellIdx;
                 end
             end
             
             forIntersection = ismember(labeledFrame, currCellIdx);
             % If the cell intersects the "start" tripwire
             if(~matchFound && any(forIntersection(tripWireStart,:)))
-                cellID(currLane) = cellID(currLane) + 1;
-                passageStatus{currLane}(cellID(currLane)) = true;
-                newEntryIdx = size(cellData{currLane}{cellID(currLane)}, 1) + 1;
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 1) = currFrameIdx;
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 2) = currCell.Centroid(1);
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 3) = currCell.Centroid(2);
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 4) = currCell.Area;
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 5) = currCell.MajorAxisLength;
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 6) = currCell.MinorAxisLength;
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 7) = currCell.BoundingBox(3);
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 8) = currCell.BoundingBox(4);
-            % Else if a match was found
-            elseif(matchFound)
-                % If the cell intersects the "end" tripwire, "cap" the
-                % cell frame entries so no more are appended for that
-                % particular cell
-                if(any(forIntersection(tripWireEnd,:)))
-                    toCap = find(passageStatus{currLane}, 1);
-                    passageStatus{currLane}(toCap) = false;
-                end
-                % Store the cell data for the frame regardless of whether
-                % it intersects the "end" tripwire
-                newEntryIdx = size(cellData{currLane}{cellID(currLane)}, 1) + 1;
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 1) = currFrameIdx;
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 2) = currCell.Centroid(1);
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 3) = currCell.Centroid(2);
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 4) = currCell.Area(1);
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 5) = currCell.MajorAxisLength;
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 6) = currCell.MinorAxisLength;
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 7) = currCell.BoundingBox(3);
-                cellData{currLane}{cellID(currLane)}(newEntryIdx, 8) = currCell.BoundingBox(4);
+                cellID(cellLane) = cellID(cellLane) + 1;
+                passageStatus{cellLane}(cellID(cellLane)) = true;
+                newEntryIdx = size(cellData{cellLane}{cellID(cellLane)}, 1) + 1;
+                cellData{cellLane}{cellID(cellLane)}(newEntryIdx, 1) = currFrameIdx;
+                cellData{cellLane}{cellID(cellLane)}(newEntryIdx, 2) = currCell.Centroid(1);
+                cellData{cellLane}{cellID(cellLane)}(newEntryIdx, 3) = currCell.Centroid(2);
+                cellData{cellLane}{cellID(cellLane)}(newEntryIdx, 4) = currCell.Area;
+                cellData{cellLane}{cellID(cellLane)}(newEntryIdx, 5) = currCell.MajorAxisLength;
+                cellData{cellLane}{cellID(cellLane)}(newEntryIdx, 6) = currCell.MinorAxisLength;
+                cellData{cellLane}{cellID(cellLane)}(newEntryIdx, 7) = currCell.BoundingBox(3);
+                cellData{cellLane}{cellID(cellLane)}(newEntryIdx, 8) = currCell.BoundingBox(4);
             end
         end
+        
+        for currLane = 1:16
+            cellsPassing = find(passageStatus{currLane});
+            for m = 1:length(cellsPassing)
+                cellIdx = cellsPassing(m);
+                if(size(cellData{currLane}{cellIdx}, 1) == 0)
+                    continue;
+                end
+                
+                lastEntry = cellData{currLane}{cellIdx}(end, :);
+                % If the last entry's frame is the current frame, then
+                % the entry is for a new cell, so skip matching it
+                if(lastEntry(1) == currFrameIdx) 
+                    continue;
+                end
+                
+                bestCellIdx = -1;
+%                 bestCellDist = 1000000;
+                matchScores = [];
+                matchScoreIdx = 1;
+                if(~isempty(potentialMatches{currLane}))
+                    toCheck = potentialMatches{currLane}(potentialMatches{currLane}(:, 1) == lastEntry(1), :);
+                    for matchIdx = 1:size(toCheck, 1)
+                        currCell = cellProps(toCheck(matchIdx, 2));
+                        distScore = norm(currCell.Centroid(1)-lastEntry(2), currCell.Centroid(2)-lastEntry(3));
+                        areaScore = abs(currCell.Area - lastEntry(4));
+                        matchScores(matchScoreIdx, 1) = toCheck(matchIdx, 2);
+                        matchScores(matchScoreIdx, 2) = 0.75*distScore + 0.25*areaScore;
+%                         if(currDist < bestCellDist)
+%                             bestCellIdx = toCheck(matchIdx, 2);
+%                             bestCellDist = currDist;
+%                             disp(['Best dist: ', num2str(bestCellDist), ' at cell ', num2str(bestCellIdx)]);
+%                         end
+                    end
+                    
+                    lowestScoreIdx = find(min(matchScores));
+                    bestCellIdx = matchScores(lowestScoreIdx, 1);
+                end
+                
+                if(bestCellIdx ~= -1)
+                    bestCell = cellProps(bestCellIdx);
+                    forIntersection = ismember(labeledFrame, bestCellIdx);
+                    % If the cell intersects the "end" tripwire, "cap" the
+                    % cell frame entries so no more are appended for that
+                    % particular cell
+                    intersectsEndTripwire = any(forIntersection(tripWireEnd,:));
+                    if(intersectsEndTripwire ||...
+                       (~intersectsEndTripwire && bestCell.Centroid(2) > tripWireEnd))
+                        toCap = find(passageStatus{currLane}, 1);
+                        passageStatus{currLane}(toCap) = false;
+                    end
+                    
+                    % Store the cell data for the frame regardless of whether
+                    % it intersects the "end" tripwire
+                    newEntryIdx = size(cellData{currLane}{cellID(currLane)}, 1) + 1;
+                    cellData{currLane}{cellIdx}(newEntryIdx, 1) = currFrameIdx;
+                    cellData{currLane}{cellIdx}(newEntryIdx, 2) = bestCell.Centroid(1);
+                    cellData{currLane}{cellIdx}(newEntryIdx, 3) = bestCell.Centroid(2);
+                    cellData{currLane}{cellIdx}(newEntryIdx, 4) = bestCell.Area(1);
+                    cellData{currLane}{cellIdx}(newEntryIdx, 5) = bestCell.MajorAxisLength;
+                    cellData{currLane}{cellIdx}(newEntryIdx, 6) = bestCell.MinorAxisLength;
+                    cellData{currLane}{cellIdx}(newEntryIdx, 7) = bestCell.BoundingBox(3);
+                    cellData{currLane}{cellIdx}(newEntryIdx, 8) = bestCell.BoundingBox(4);
+                end
+            end  
+        end    
     end
     
     if(WRITEVIDEO_FLAG)
