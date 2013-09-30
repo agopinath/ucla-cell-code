7b6fa26f61ad1e741b3a6e2bb034f9392b34f36e
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-09-30 00:23:25 -0700

    modified constrictions vs deformation plotting script to properly account for diff. between two unconstricted cell frames

--- AnalysisScripts/CellPerim_ConstrXDeformation.m
+++ AnalysisScripts/CellPerim_ConstrXDeformation.m
@@ -16,9 +16,16 @@ for currLane = 1:16
                 continue;
             end
             postCon = conEnd + 1;
-            
-            conEnd = currCellPerim{conEnd}(:, 2);
-            postCon = currCellPerim{postCon}(:, 2);
+            if(postCon > numFrames || numFrames < 2)
+                continue;
+            end
+            if(currCon ~= 1)
+                conEnd = currCellPerim{conEnd}(:, 2);
+                postCon = currCellPerim{postCon}(:, 2);
+            else
+                conEnd = currCellPerim{1}(:, 2);
+                postCon = currCellPerim{2}(:, 2);
+            end
             defDeltas(currCon) = sum(abs(postCon - conEnd));
             deltas{currCon}(end+1) = defDeltas(currCon);
         end
@@ -26,6 +33,12 @@ for currLane = 1:16
     end
 end
 hold off;
+mn = ceil(mean(arrayfun(@(x) length(deltas{x}), xCon)));
+for uu = 1:length(xCon)
+    while(length(deltas{uu}) < mn)
+        deltas{uu}(end+1) = NaN;
+    end
+end
 deltas = horzcat(deltas{1}', deltas{2}', deltas{3}', deltas{4}', deltas{5}',...
                  deltas{6}', deltas{7}', deltas{8}');
 boxplot(deltas);
