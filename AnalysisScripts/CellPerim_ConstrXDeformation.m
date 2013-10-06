53e834d9ebf8b9baeead315e58345dfd64c7e9c0
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-10-05 19:58:51 -0700

    fixing conVSDiffs plot script

--- AnalysisScripts/CellPerim_ConstrXDeformation.m
+++ AnalysisScripts/CellPerim_ConstrXDeformation.m
@@ -9,25 +9,52 @@ for currLane = 1:16
         
         numFrames = size(currCell, 1);
         for currCon = 1:8
-            conEnd = find(currCell(:,9) == currCon, 1, 'last');
+            if(currCon ~= 1)
+                conEnd = find(currCell(:,9) == currCon, 1, 'last');
+            else
+                conEnd = find(currCell(:,9) == currCon, 1, 'first');
+            end
+            
             if(isempty(conEnd))
                 defDeltas(currCon) = NaN;
                 deltas{currCon}(end+1) = NaN;
                 continue;
             end
-            postCon = conEnd + 1;
-            if(postCon > numFrames || numFrames < 2)
+            
+            if(currCon ~= 1)
+                postCon = find(cellData{currLane}{currCellIdx}(:, 9) == i+0.5, 1, 'last');
+            else
+                postCon = find(cellData{currLane}{currCellIdx}(:, 9) == i, 1, 'last');
+            end
+            
+            if(isempty(postCon) || postCon > numFrames || numFrames < 2 )
+                defDeltas(currCon) = NaN;
+                deltas{currCon}(end+1) = NaN;
                 continue;
             end
-            if(currCon ~= 1)
-                conEnd = currCellPerim{conEnd}(:, 2);
-                postCon = currCellPerim{postCon}(:, 2);
+            
+            postConStart = conEnd+1;
+            if(postCon - postConStart > 3)
+                postConEnd = postConStart+3;
             else
-                conEnd = currCellPerim{1}(:, 2);
-                postCon = currCellPerim{2}(:, 2);
+                postConEnd = postCon;
             end
-            defDeltas(currCon) = sum(abs(postCon - conEnd));
-            deltas{currCon}(end+1) = defDeltas(currCon);
+            
+            postConWave = 0;
+            for postConIdx = postConStart:postConEnd
+                currYs = cellPerimsData{currLane}{currCellIdx}{:, postConIdx};
+                if(isempty(currYs))
+                    continue;
+                end
+                postConWave = postConWave + currYs(:, 2);
+            end
+            postConWave = postConWave/(postConEnd-postConStart+1);
+            postConWave = postConWave/cellData{currLane}{currCellIdx}(1, 4); % normalize by dividing by uncon. cell area
+            conEndWave = cellPerimsData{currLane}{currCellIdx}{:, conEnd}(:, 2);
+            conEndWave = conEndWave/cellData{currLane}{currCellIdx}(1, 4); % normalize by dividing by uncon. cell area
+            
+            defDeltas(currCon) = sum(abs(postConWave - conEndWave));
+            deltas{currCon}(end+1) = defDeltas(currCon)';
         end
         
     end
